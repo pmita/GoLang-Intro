5731bdf857e046a33955f0f4b063bf06128d8174
pmita <56344856+pmita@users.noreply.github.com>
2019-10-25 16:32:38 +0100

    Update Flightman.go
    This script gets data from flightStats API in accordance with the coordinates we sent them. At this stage the coordinates are fixed on the table. Next step would be sending the data from DarkSky API. Last step would be to implement the current date and scope through incoming data for appropriate time frames.

--- Flightman.go
+++ Flightman.go
@@ -29,21 +29,7 @@ type FlightData struct{
 //var airport string
 
 func main(){
-	/*var d FlightData
-	citiesToVisit := [3]string{"ATH", "FRA", "MXP"}
 	
-	response, err :=http.Get("https://api.flightstats.com/flex/schedules/rest/v1/json/from/LHR/to/ATH/departing/2020/03/24?appId=605d73d7&appKey=78a37d155d4f07e5f891b8a609f50d59&codeType=IATA")
-	if err != nil{
-		fmt.Println("Failed HTTP request %s\n", err)
-	}
-
-	defer response.Body.Close()
-	
-	if err := json.NewDecoder(response.Body).Decode(&d); err !=nil{
-		fmt.Println( FlightData{}, err)
-	} else {
-		fmt.Println(d)
-	}*/
 	var d FlightData
 	citiesToVisit := [3]string{"ATH", "FRA", "MLN"}
 	var citiesToVisitURLs = fmt.Sprintf("https://api.flightstats.com/flex/schedules/rest/v1/json/from/LHR/to/%s/departing/2020/03/24?appId=605d73d7&appKey=78a37d155d4f07e5f891b8a609f50d59&codeType=IATA", citiesToVisit[1])
@@ -52,7 +38,7 @@ func main(){
 	if err != nil {
 			return
 		}
-
+	/*The base.Parse code isn't vaiable since the character ? translates to %3F when combining the paths. Need more debugging*/
 	base.Path += citiesToVisit[1]
 	base.Path += "/departing/2020/03/24/"
 	base.Path += "?"
@@ -80,41 +66,4 @@ func main(){
 	
 }
 	
-	/*var d FlightData
-	citiesToVisit := [3]string{"ATH", "FRA", "MXP"}
-	base, err := url.Parse("https://api.flightstats.com/flex/schedules/rest/v1/json/from/LHR/to/")
-	if err != nil {
-			return
-		}
-
-	base.Path += citiesToVisit[0]
-	base.Path += "/departing/2020/03/24?appId=605d73d7&appKey=78a37d155d4f07e5f891b8a609f50d59&codeType=IATA"
-
-	response, err :=http.Get(base.String())
-	if err != nil{
-		fmt.Println("Failed HTTP request %s\n", err)
-	}
-
-	defer response.Body.Close()
-	
-	if err := json.NewDecoder(response.Body).Decode(&d); err !=nil{
-		fmt.Println( FlightData{}, err)
-	} else {
-		fmt.Println(d)
-	}
-	
-	for i:=0; i<=len(airports); i++{*/
-	
-	
-	// Path params
-	
-	
-
-	// Query params
-	//params := url.Values{}
-	//params.Add("q", "this will get encoded as well")
-	//base.RawQuery = params.Encode() 
-	
-	//fmt.Printf("Encoded URL is %q\n", base.String())
-	//}
 
